715c35d5df311b5edc6dbbbba03d0fc3f054c159
Unknown <aeon.williams@digipen.edu>
2018-10-01 13:43:29 -0700

    WIP DRAG-695
    More block documentation added.

--- src/generators/javascript/phaser.js
+++ src/generators/javascript/phaser.js
@@ -2370,20 +2370,55 @@ Blockly.JavaScript['get_active_pointer'] = function (block) {
 //endregion
 
 //region Keyboard
+/**
+ * @deprecated
+ * @param block
+ * @returns {}
+ */
 Blockly.JavaScript['create_cursor_keys'] = function (block) {
   return ['game.input.keyboard.createCursorKeys()', Blockly.JavaScript.ORDER_NONE];
 };
 
+/**
+ * Returns true/false about whether or not the given key is currently being pressed down.
+ * @method is_key_down
+ * @param key key to check
+ * @returns {} game.input.keyboard.isDown(Phaser.Keyboard.key)
+ * @block
+ */
 Blockly.JavaScript['is_key_down'] = function (block) {
-  const dropdown_key = block.getFieldValue('KEY');
-  return [`game.input.keyboard.isDown(Phaser.Keyboard.${dropdown_key})`, Blockly.JavaScript.ORDER_NONE];
+  const key = block.getFieldValue('KEY');
+  return [`game.input.keyboard.isDown(Phaser.Keyboard.${key})`, Blockly.JavaScript.ORDER_NONE];
 };
 
+/**
+ * Creates a Phaser key object.
+ * @method add_key
+ * @param key keycode of the key to create
+ * @returns {} game.input.keyboard.addKey(Phaser.Keyboard.key)
+ * @block
+ */
 Blockly.JavaScript['add_key'] = function (block) {
-  const dropdown_key = block.getFieldValue('KEYCODE');
-  return [`game.input.keyboard.addKey(Phaser.Keyboard.${dropdown_key})`, Blockly.JavaScript.ORDER_ATOMIC];
+  const key = block.getFieldValue('KEYCODE');
+  return [`game.input.keyboard.addKey(Phaser.Keyboard.${key})`, Blockly.JavaScript.ORDER_ATOMIC];
 };
 
+/**
+ * Returns the chosen boolean field value of the key.
+ * @method get_key_boolean_field
+ * @param field the value to get
+ * @param key key to get values from
+ * @returns {} key.field
+ * @block
+ */
+/**
+ * Returns the chosen numeric field value of the key.
+ * @method get_key_numeric_field
+ * @param field the value to get
+ * @param key key to get values from
+ * @returns {} key.field
+ * @block
+ */
 Blockly.JavaScript['get_key_boolean_field']
   = Blockly.JavaScript['get_key_numeric_field']
   = function (block) {
@@ -2392,28 +2427,66 @@ Blockly.JavaScript['get_key_boolean_field']
   return [`${key}.${field}`, Blockly.JavaScript.ORDER_ATOMIC];
 };
 
+/**
+ * Returns true/false if the given key was pressed down this update tick.
+ * @method key_just_pressed
+ * @param key key to check
+ * @returns {} key.justPressed()
+ * @block
+ */
 Blockly.JavaScript['key_just_pressed'] = function (block) {
   const key = Blockly.JavaScript.valueToCode(block, 'KEY', Blockly.JavaScript.ORDER_ATOMIC);
   return [`${key}.justPressed()`, Blockly.JavaScript.ORDER_ATOMIC];
 };
 
+/**
+ * Returns true/false if the given key was released down this update tick.
+ * @method key_just_released
+ * @param key key to check
+ * @returns {} key.justReleased()
+ * @block
+ */
 Blockly.JavaScript['key_just_released'] = function (block) {
   const key = Blockly.JavaScript.valueToCode(block, 'KEY', Blockly.JavaScript.ORDER_ATOMIC);
   return [`${key}.justReleased()`, Blockly.JavaScript.ORDER_ATOMIC];
 };
 
+/**
+ * Resets the state of the key. Hard reset removes any callback methods associated with it.
+ * @method key_reset
+ * @param key key to reset
+ * @param hard {Boolean} whether or not to hard reset the key
+ * @returns {} key.reset(hard)
+ * @block
+ */
 Blockly.JavaScript['key_reset'] = function (block) {
   const key = Blockly.JavaScript.valueToCode(block, 'KEY', Blockly.JavaScript.ORDER_ATOMIC);
   const hard = block.getFieldValue('HARD') == 'TRUE';
   return `${key}.reset(${hard});\n`;
 };
 
+/**
+ * Returns true/false if the given key was released within the given amount of time.
+ * @method key_up_duration
+ * @param key key to check
+ * @param duration {Number} duration within which the key is considered as just released
+ * @returns {} key.upDuration(duration);
+ * @block
+ */
 Blockly.JavaScript['key_up_duration'] = function (block) {
   const key = Blockly.JavaScript.valueToCode(block, 'KEY', Blockly.JavaScript.ORDER_ATOMIC);
   const duration = Blockly.JavaScript.valueToCode(block, 'DURATION', Blockly.JavaScript.ORDER_ATOMIC);
   return [`${key}.upDuration(${duration})`, Blockly.JavaScript.ORDER_ATOMIC];
 };
 
+/**
+ * Returns true/false if the given key was pressed within the given amount of time.
+ * @method key_up_duration
+ * @param key key to check
+ * @param duration {Number} duration within which the key is considered as just pressed
+ * @returns {} key.downDuration(duration);
+ * @block
+ */
 Blockly.JavaScript['key_down_duration'] = function (block) {
   const key = Blockly.JavaScript.valueToCode(block, 'KEY', Blockly.JavaScript.ORDER_ATOMIC);
   const duration = Blockly.JavaScript.valueToCode(block, 'DURATION', Blockly.JavaScript.ORDER_ATOMIC);
@@ -2422,27 +2495,85 @@ Blockly.JavaScript['key_down_duration'] = function (block) {
 //endregion
 
 //region INPUT_HANDLER
+/**
+ * Enables an object to use input handler methods and events.
+ * @method input_handler_enable
+ * @param object object to enable the input handler on
+ * @returns {} object.inputEnabled = true;
+ * @block
+ */
 Blockly.JavaScript['input_handler_enable'] = function (block) {
   const object = Blockly.JavaScript.valueToCode(block, 'OBJECT', Blockly.JavaScript.ORDER_ATOMIC);
   return `${object}.inputEnabled = true;\n`;
 };
 
+/**
+ * Assigns the chosen boolean field for the object.
+ * @method set_input_handler_boolean_field
+ * @param field the value to set
+ * @param object object to assign values for
+ * @param value value to set the field to
+ * @returns {} object.input.field = value;
+ * @block
+ */
+/**
+ * Assigns the chosen numeric field for the object.
+ * @method set_input_handler_numeric_field
+ * @param field the value to set
+ * @param object object to assign values for
+ * @param value value to set the field to
+ * @returns {} object.input.field = value;
+ * @block
+ */
 Blockly.JavaScript['set_input_handler_boolean_field']
   = Blockly.JavaScript['set_input_handler_numeric_field']
   = function (block) {
   const field = block.getFieldValue('FIELD');
   const object = Blockly.JavaScript.valueToCode(block, 'OBJECT', Blockly.JavaScript.ORDER_ATOMIC);
-  const newProperty = Blockly.JavaScript.valueToCode(block, 'VALUE', Blockly.JavaScript.ORDER_ATOMIC);
-  return `${object}.input.${field} = ${newProperty};\n`;
+  const value = Blockly.JavaScript.valueToCode(block, 'VALUE', Blockly.JavaScript.ORDER_ATOMIC);
+  return `${object}.input.${field} = ${value};\n`;
 };
 
+/**
+ * Assigns the chosen point field for the object.
+ * @method set_input_handler_point_field
+ * @param field the value to set
+ * @param object object to assign values for
+ * @param value value to set the field to
+ * @returns {} object.input.field.copyFrom(value);
+ * @block
+ */
 Blockly.JavaScript['set_input_handler_point_field'] = function (block) {
   const field = block.getFieldValue('FIELD');
   const object = Blockly.JavaScript.valueToCode(block, 'OBJECT', Blockly.JavaScript.ORDER_ATOMIC);
-  const newProperty = Blockly.JavaScript.valueToCode(block, 'VALUE', Blockly.JavaScript.ORDER_ATOMIC);
-  return `${object}.input.${field}.copyFrom(${newProperty});\n`;
+  const value = Blockly.JavaScript.valueToCode(block, 'VALUE', Blockly.JavaScript.ORDER_ATOMIC);
+  return `${object}.input.${field}.copyFrom(${value});\n`;
 };
 
+/**
+ * Returns the chosen boolean field value of the object.
+ * @method get_key_boolean_field
+ * @param field the value to get
+ * @param object object to get values from
+ * @returns {} object.input.field
+ * @block
+ */
+/**
+ * Returns the chosen numeric field value of the object.
+ * @method get_key_numeric_field
+ * @param field the value to get
+ * @param object object to get values from
+ * @returns {} object.input.field
+ * @block
+ */
+/**
+ * Returns the chosen point field value of the object.
+ * @method get_key_point_field
+ * @param field the value to get
+ * @param object object to get values from
+ * @returns {} object.input.field
+ * @block
+ */
 Blockly.JavaScript['get_input_handler_boolean_field']
   = Blockly.JavaScript['get_input_handler_numeric_field']
   = Blockly.JavaScript['get_input_handler_point_field']
@@ -2452,17 +2583,43 @@ Blockly.JavaScript['get_input_handler_boolean_field']
   return [`${object}.input.${field}`, Blockly.JavaScript.ORDER_ATOMIC];
 };
 
+/**
+ * Sets the boundary of where the sprite is restricted while being dragged.
+ * @method input_handler_bounds_rect_set
+ * @param object object to set the boundary for
+ * @param rect rectangle that defines the boundary
+ * @returns {} object.input.boundsRect = rect;
+ * @block
+ */
 Blockly.JavaScript['input_handler_bounds_rect_set'] = function (block) {
   const object = Blockly.JavaScript.valueToCode(block, 'OBJECT', Blockly.JavaScript.ORDER_ATOMIC);
   const rect = Blockly.JavaScript.valueToCode(block, 'RECT', Blockly.JavaScript.ORDER_ATOMIC);
   return `${object}.input.boundsRect = ${rect};\n`;
 };
 
+/**
+ * Allows the object to be clicked and dragged by the mouse.
+ * @method input_handler_enable_drag
+ * @param object object to enable dragging on
+ * @returns {} object.input.enableDrag();
+ * @block
+ */
 Blockly.JavaScript['input_handler_enable_drag'] = function (block) {
   const object = Blockly.JavaScript.valueToCode(block, 'OBJECT', Blockly.JavaScript.ORDER_ATOMIC);
   return `${object}.input.enableDrag();\n`;
 };
 
+/**
+ * Allows the object to be clicked and dragged by the mouse, with customizable options.
+ * @method input_handler_enable_drag_complex
+ * @param object object to enable dragging on
+ * @param center {Boolean} If false the Sprite will drag from where you click it minus the dragOffset. If true it will center itself to the tip of the mouse pointer
+ * @param top {Boolean} If true the object will be displayed on top of everything else
+ * @param pixel {Boolean} If true it will use a pixel perfect test to see if you clicked the object. False uses the bounding box
+ * @param alpha {Boolean} If pixel perfect is true, this specifies the alpha level needed for collision to be processed
+ * @returns {} object.input.enableDrag(center, top, pixel, alpha);
+ * @block
+ */
 Blockly.JavaScript['input_handler_enable_drag_complex'] = function (block) {
   const object = Blockly.JavaScript.valueToCode(block, 'OBJECT', Blockly.JavaScript.ORDER_ATOMIC);
   const center = block.getFieldValue('CENTER') === 'TRUE';
@@ -2472,23 +2629,55 @@ Blockly.JavaScript['input_handler_enable_drag_complex'] = function (block) {
   return `${object}.input.enableDrag(${center}, ${top}, ${pixel}, ${alpha});\n`;
 };
 
+/**
+ * Returns true/false if the pointer is currently clicking on the object.
+ * @method input_handler_check_pointer_down
+ * @param pointer pointer to check
+ * @param object object to check
+ * @returns {} input.checkPointerDown(pointer)
+ * @block
+ */
 Blockly.JavaScript['input_handler_check_pointer_down'] = function (block) {
   const pointer = Blockly.JavaScript.valueToCode(block, 'POINTER', Blockly.JavaScript.ORDER_ATOMIC);
   const object = Blockly.JavaScript.valueToCode(block, 'OBJECT', Blockly.JavaScript.ORDER_ATOMIC);
   return [`${object}.input.checkPointerDown(${pointer})`, Blockly.JavaScript.ORDER_ATOMIC];
 };
 
+/**
+ * Returns true/false if the pointer is on top of the object.
+ * @method input_handler_check_pointer_over
+ * @param pointer pointer to check
+ * @param object object to check
+ * @returns {} object.input.checkPointerOver(pointer)
+ * @block
+ */
 Blockly.JavaScript['input_handler_check_pointer_over'] = function (block) {
   const pointer = Blockly.JavaScript.valueToCode(block, 'POINTER', Blockly.JavaScript.ORDER_ATOMIC);
   const object = Blockly.JavaScript.valueToCode(block, 'OBJECT', Blockly.JavaScript.ORDER_ATOMIC);
   return [`${object}.input.checkPointerOver(${pointer})`, Blockly.JavaScript.ORDER_ATOMIC];
 };
 
+/**
+ * Stops the object from being dragged. If it is currently being dragged, it will be stopped immediately.
+ * @method input_handler_disable_drag
+ * @param object object to disable drag on
+ * @returns {} object.input.disableDrag();
+ * @block
+ */
 Blockly.JavaScript['input_handler_disable_drag'] = function (block) {
   const object = Blockly.JavaScript.valueToCode(block, 'OBJECT', Blockly.JavaScript.ORDER_ATOMIC);
   return `${object}.input.disableDrag();\n`;
 };
 
+/**
+ * Make the object snap to the given x/y grid when being dragged or released.
+ * @method input_handler_enable_snap
+ * @param object object to snap
+ * @param x {Number} the width of the grid to snap to
+ * @param y {Number} the height of the grid to snap to
+ * @returns {} object.input.enableSnap(x, y);
+ * @block
+ */
 Blockly.JavaScript['input_handler_enable_snap'] = function (block) {
   const object = Blockly.JavaScript.valueToCode(block, 'OBJECT', Blockly.JavaScript.ORDER_ATOMIC);
   const x = Blockly.JavaScript.valueToCode(block, 'X', Blockly.JavaScript.ORDER_ATOMIC);
@@ -2497,6 +2686,19 @@ Blockly.JavaScript['input_handler_enable_snap'] = function (block) {
   return `${object}.input.enableSnap(${x}, ${y});\n`;
 };
 
+/**
+ * Make the object snap to the given x/y grid when being dragged or released, with customizable options.
+ * @method input_handler_enable_snap_complex
+ * @param object object to snap
+ * @param x {Number} the width of the grid to snap to
+ * @param y {Number} the height of the grid to snap to
+ * @param drag {Boolean} makes the object snap to the grid while being dragged
+ * @param release {Boolean} makes the object snap to the grid when released
+ * @param offset_x {Number} offsets the top left starting point of the grid
+ * @param offset_y {Number} offsets the top left starting point of the grid
+ * @returns {} object.input.enableSnap(x, y, drag, release, offset_x, offset_y);
+ * @block
+ */
 Blockly.JavaScript['input_handler_enable_snap_complex'] = function (block) {
   const object = Blockly.JavaScript.valueToCode(block, 'OBJECT', Blockly.JavaScript.ORDER_ATOMIC);
   const drag = block.getFieldValue('DRAG') === 'TRUE';
@@ -2509,11 +2711,27 @@ Blockly.JavaScript['input_handler_enable_snap_complex'] = function (block) {
   return `${object}.input.enableSnap(${x}, ${y}, ${drag}, ${release}, ${offset_x}, ${offset_y});\n`;
 };
 
+/**
+ * Stops the object from snapping to a grid while being dragged and released.
+ * @method input_handler_disable_snap
+ * @param object object to disable snap for
+ * @returns {} object.input.disableSnap();
+ * @block
+ */
 Blockly.JavaScript['input_handler_disable_snap'] = function (block) {
   const object = Blockly.JavaScript.valueToCode(block, 'OBJECT', Blockly.JavaScript.ORDER_ATOMIC);
   return `${object}.input.disableSnap();\n`;
 };
 
+/**
+ * Returns true/false if the alpha value at the given location is greater than or equal to the object's pixelPerfectAlpha value.
+ * @method input_handler_check_pixel
+ * @param x {Number} x coordinate to check
+ * @param y {Number} y coordinate to check
+ * @param object object to check
+ * @returns {} object.input.checkPixel(x, y)
+ * @block
+ */
 Blockly.JavaScript['input_handler_check_pixel'] = function (block) {
   const x = Blockly.JavaScript.valueToCode(block, 'X', Blockly.JavaScript.ORDER_ATOMIC);
   const y = Blockly.JavaScript.valueToCode(block, 'Y', Blockly.JavaScript.ORDER_ATOMIC);
@@ -2521,44 +2739,101 @@ Blockly.JavaScript['input_handler_check_pixel'] = function (block) {
   return [`${object}.input.checkPixel(${x}, ${y})`, Blockly.JavaScript.ORDER_ATOMIC];
 };
 
+/**
+ * Returns true/false if the object is using pixel perfect checking.
+ * @method input_handler_is_pixel_perfect
+ * @param object object to check
+ * @returns {} object.input.isPixelPerfect()
+ * @block
+ */
 Blockly.JavaScript['input_handler_is_pixel_perfect'] = function (block) {
   const object = Blockly.JavaScript.valueToCode(block, 'OBJECT', Blockly.JavaScript.ORDER_ATOMIC);
   return [`${object}.input.isPixelPerfect()`, Blockly.JavaScript.ORDER_ATOMIC];
 };
 
+/**
+ * Returns true/false if the object was clicked on within the given time.
+ * @method input_handler_just_pressed
+ * @param object object to check
+ * @param time {Number} the duration to check
+ * @returns {} object.input.justPressed(0, time)
+ * @block
+ */
 Blockly.JavaScript['input_handler_just_pressed'] = function (block) {
   const object = Blockly.JavaScript.valueToCode(block, 'OBJECT', Blockly.JavaScript.ORDER_ATOMIC);
   const time = Blockly.JavaScript.valueToCode(block, 'TIME', Blockly.JavaScript.ORDER_ATOMIC);
   return [`${object}.input.justPressed(0, ${time})`, Blockly.JavaScript.ORDER_ATOMIC];
 };
 
+/**
+ * Returns true/false if the object was released on within the given time.
+ * @method input_handler_just_released
+ * @param object object to check
+ * @param time {Number} the duration to check
+ * @returns {} object.input.justReleased(0, time)
+ * @block
+ */
 Blockly.JavaScript['input_handler_just_released'] = function (block) {
   const object = Blockly.JavaScript.valueToCode(block, 'OBJECT', Blockly.JavaScript.ORDER_ATOMIC);
   const time = Blockly.JavaScript.valueToCode(block, 'TIME', Blockly.JavaScript.ORDER_ATOMIC);
   return [`${object}.input.justReleased(0, ${time})`, Blockly.JavaScript.ORDER_ATOMIC];
 };
 
+/**
+ * Returns the number of milliseconds the mouse has been hovering over the object.
+ * @method input_handler_over_duration
+ * @param object object to check
+ * @returns {} object.input.overDuration()
+ * @block
+ */
 Blockly.JavaScript['input_handler_over_duration'] = function (block) {
   const object = Blockly.JavaScript.valueToCode(block, 'OBJECT', Blockly.JavaScript.ORDER_ATOMIC);
   return [`${object}.input.overDuration()`, Blockly.JavaScript.ORDER_ATOMIC];
 };
 
+/**
+ * @deprecated
+ * @param block
+ * @returns {}
+ */
 Blockly.JavaScript['input_handler_pointer_over'] = function (block) {
   const object = Blockly.JavaScript.valueToCode(block, 'OBJECT', Blockly.JavaScript.ORDER_ATOMIC);
   return [`${object}.input.pointerOver()`, Blockly.JavaScript.ORDER_ATOMIC];
 };
 
+/**
+ * Returns true/false if the mouse has entered the object's bounds within the given time.
+ * @method input_handler_just_over
+ * @param object object to check
+ * @param time {Number} time to check
+ * @returns {} object.input.justOver(0, time)
+ * @block
+ */
 Blockly.JavaScript['input_handler_just_over'] = function (block) {
   const object = Blockly.JavaScript.valueToCode(block, 'OBJECT', Blockly.JavaScript.ORDER_ATOMIC);
   const time = Blockly.JavaScript.valueToCode(block, 'TIME', Blockly.JavaScript.ORDER_ATOMIC);
   return [`${object}.input.justOver(0, ${time})`, Blockly.JavaScript.ORDER_ATOMIC];
 };
 
+/**
+ * Returns the number of milliseconds the mouse has been clicking on the object.
+ * @method input_handler_down_duration
+ * @param object object to check
+ * @returns {} object.input.downDuration()
+ * @block
+ */
 Blockly.JavaScript['input_handler_down_duration'] = function (block) {
   const object = Blockly.JavaScript.valueToCode(block, 'OBJECT', Blockly.JavaScript.ORDER_ATOMIC);
   return [`${object}.input.downDuration()`, Blockly.JavaScript.ORDER_ATOMIC];
 };
 
+/**
+ * 
+ * @method
+ * @param
+ * @returns {}
+ * @block
+ */
 Blockly.JavaScript['input_handler_pointer_up'] = function (block) {
   const object = Blockly.JavaScript.valueToCode(block, 'OBJECT', Blockly.JavaScript.ORDER_ATOMIC);
   return [`${object}.input.pointerUp()`, Blockly.JavaScript.ORDER_ATOMIC];
